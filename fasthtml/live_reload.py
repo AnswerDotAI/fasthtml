02db6852cf30df8432dc8d8cc62bf195f6a9ed3c
Tommy <tcacollins@gmail.com>
2024-06-10 13:55:34 +0100

    add retries to reload script

--- fasthtml/live_reload.py
+++ fasthtml/live_reload.py
@@ -3,17 +3,24 @@ from fasthtml import FastHTML, Script
 
 __all__ = ["FastHTMLWithLiveReload"]
 
-# TODO:
-#  making a single attempt to reload the browser 1 second after the `onclose` event
-#  is pretty basic. Let's look at some existing implementations and see if we can
-#  make the reload more robust.
+
 LIVE_RELOAD_SCRIPT = """
     (function() {
         var socket = new WebSocket(`ws://${window.location.host}/live-reload`);
-        socket.onclose = function() {setTimeout(window.location.reload(), 1000)};
+        var maxReloadAttempts = 20;
+        var reloadInterval = 250; // time between reload attempts in ms
+        socket.onclose = function() {
+            let reloadAttempts = 0;
+            const intervalFn = setInterval(function(){
+                window.location.reload();
+                reloadCount++;
+                if (reloadAttempts === maxReloadAttempts) {
+                    clearInterval(intervalFn);
+                };
+            }, reloadInterval);
+        }
     })();
 """
-
 
 async def live_reload_websocket(websocket):
     await websocket.accept()
@@ -29,15 +36,12 @@ class FastHTMLWithLiveReload(FastHTML):
       - a websocket is creaetd at `/live-reload`
       - a small js snippet `LIVE_RELOAD_SCRIPT` is injected into each webpage
       - this snippet connects to the websocket at `/live-reload` and listens for an `onclose` event
-      - when the onclose event is detected the browser reloads after 1 second
+      - when the onclose event is detected the browser is reloaded
 
     Why do we listen for an `onclose` event?
       When code changes are saved the server automatically reloads if the --reload flag is set.
       The server reload kills the websocket connection. The `onclose` event serves as a proxy
       for "developer has saved some changes".
-
-    Why do we wait for 1 second before reloading the browser?
-      We need to allow some time for uvicorn to reload and start serving the latest changes.
 
     Usage
         >>> from fasthtml.all import *
