82efb5de2f70f24a1e8cafd613bf665a7fba2fdb
Jeremy Howard <github@jhoward.fastmail.fm>
2024-06-18 01:23:07 +1000

    fasthtml/svg.py

---
+++ fasthtml/svg.py
@@ -0,0 +1,4 @@
+from fasthtml.components import xt_hx, Svg, AltGlyph, AltGlyphDef, AltGlyphItem, Animate, AnimateColor, AnimateMotion, AnimateTransform, Circle, ClipPath, Color_profile, Cursor, Defs, Desc, Ellipse, FeBlend, FeColorMatrix, FeComponentTransfer, FeComposite, FeConvolveMatrix, FeDiffuseLighting, FeDisplacementMap, FeDistantLight, FeFlood, FeFuncA, FeFuncB, FeFuncG, FeFuncR, FeGaussianBlur, FeImage, FeMerge, FeMergeNode, FeMorphology, FeOffset, FePointLight, FeSpecularLighting, FeSpotLight, FeTile, FeTurbulence, Filter, Font, Font_face, Font_face_format, Font_face_name, Font_face_src, Font_face_uri, ForeignObject, G, Glyph, GlyphRef, Hkern, Image, Line, LinearGradient, Marker, Mask, Metadata, Missing_glyph, Mpath, Pattern, Polygon, Polyline, RadialGradient, Rect, Set, Stop, Switch, Symbol, Text, TextPath, Tref, Tspan, Use, View, Vkern
+
+def Path(*args, **kwargs): return xt_hx('path', *args, **kwargs)
+
