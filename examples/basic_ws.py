71b4fd44c8511d2463b7952a72cb8c3e0e1f401c
Jeremy Howard <github@jhoward.fastmail.fm>
2024-07-04 14:01:01 +1000

    format

--- examples/basic_ws.py
+++ examples/basic_ws.py
@@ -5,26 +5,24 @@ app = FastHTML(ws_hdr=True)
 rt = app.route
 
 def mk_inp(): return Input(id='msg')
+nid = 'notifications'
 
 @rt('/')
 async def get(request):
     cts = Div(
-        Div(id='notifications'),
+        Div(id=nid),
         Form(mk_inp(), id='form', ws_send=True),
         hx_ext='ws', ws_connect='/ws')
     return Titled('Websocket Test', cts)
 
-async def on_connect(send):
-    print('Connected!')
-    await send(Div('Hello, you have connected', id="notifications"))
-
-async def on_disconnect(ws):
-    print('Disconnected!')
+async def on_connect(send): await send(Div('Hello, you have connected', id=nid))
+async def on_disconnect( ): print('Disconnected!')
 
 @app.ws('/ws', conn=on_connect, disconn=on_disconnect)
 async def ws(msg:str, send):
-    await send(Div('Hello ' + msg, id="notifications"))
+    await send(Div('Hello ' + msg, id=nid))
     await sleep(2)
-    return Div('Goodbye ' + msg, id="notifications"), mk_inp()
+    return Div('Goodbye ' + msg, id=nid), mk_inp()
 
 run_uv()
+
