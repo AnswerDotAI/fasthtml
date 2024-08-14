3e5b263ce6df7962e606b8aaefe93bb92db7c9df
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2024-08-14 12:35:28 +1000

    Remove clock class, don't need it
    Co-authored-by: Daniel Roy Greenfeld <daniel@feldroy.com>

--- examples/sse_clock.py
+++ examples/sse_clock.py
@@ -9,12 +9,12 @@ app, rt = fast_app(hdrs=(sselink,))
 @rt("/")
 def get():
     return Titled("SSE Clock",
-            P("XX:XX", cls="clock", sse_swap="TimeUpdateEvent",
+            P("XX:XX", sse_swap="TimeUpdateEvent",
             hx_ext="sse", sse_connect="/time-sender"))
 
 async def time_generator():
     while True:
-        yield f"""event: TimeUpdateEvent\ndata: {to_xml(P(datetime.now().strftime('%H:%M:%S'), cls="clock", sse_swap="TimeUpdateEvent"))}\n\n"""
+        yield f"""event: TimeUpdateEvent\ndata: {to_xml(P(datetime.now().strftime('%H:%M:%S'), sse_swap="TimeUpdateEvent"))}\n\n"""
         await asyncio.sleep(1)
 
 @rt("/time-sender")
