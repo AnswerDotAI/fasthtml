aa10ee5a03a4a12f4162adfafe1d296c5b1826c6
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2024-08-14 12:26:15 +1000

    Add simple SSE example
    * Time generator yields a `TimeUpdateEvent` every second
    * SSE server endpoint `/time-sender` sends out the time every second
    * Client `/` updates a `P` with the time on each `TimeUpdateEvent`

---
+++ examples/sse_clock.py
@@ -0,0 +1,25 @@
+import asyncio
+from datetime import datetime
+from fasthtml.common import *
+from starlette.responses import StreamingResponse
+
+sselink = Script(src="https://unpkg.com/htmx-ext-sse@2.2.1/sse.js")
+app, rt = fast_app(hdrs=(sselink,))
+
+@rt("/")
+def get():
+    return Titled("SSE Clock",
+            P("XX:XX", cls="clock", sse_swap="TimeUpdateEvent",
+            hx_ext="sse", sse_connect="/time-sender"))
+
+async def time_generator():
+    while True:
+        yield f"""event: TimeUpdateEvent\ndata: {to_xml(P(datetime.now().strftime('%H:%M:%S'), cls="clock", sse_swap="TimeUpdateEvent"))}\n\n"""
+        await asyncio.sleep(1)
+
+@rt("/time-sender")
+async def get():
+    "Send time to all connected clients every second"
+    return StreamingResponse(time_generator(), media_type="text/event-stream")
+
+serve()
