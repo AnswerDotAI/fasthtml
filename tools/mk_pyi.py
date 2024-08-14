5f6adfd484ff693afd6d1334e618f0ab81e4378a
Jeremy Howard <github@jhoward.fastmail.fm>
2024-08-14 10:19:43 +1000

    fixes #255

--- tools/mk_pyi.py
+++ tools/mk_pyi.py
@@ -2,15 +2,13 @@ from fasthtml.common import *
 from fasthtml.components import _all_
 from fastcore.py2pyi import create_pyi
 
-both_attrs = html_attrs+hx_attrs
-
 create_pyi('fasthtml/components.py', 'fasthtml')
 create_pyi('fasthtml/xtend.py', 'fasthtml')
 with open('fasthtml/components.pyi', 'a') as f:
-    attrs_str = ', '.join(f'{t}:Any=None' for t in both_attrs)
+    attrs_str = ', '.join(f'{t}:Any=None' for t in hx_attrs)
     f.write(f"\ndef ft_html(tag: str, *c, {attrs_str}, **kwargs): ...\n")
     f.write(f"def ft_hx(tag: str, *c, {attrs_str}, **kwargs): ...\n")
     for o in _all_:
-        attrs = (['name'] if o.lower() in named else []) + both_attrs
+        attrs = (['name'] if o.lower() in named else []) + hx_attrs
         attrs_str = ', '.join(f'{t}:Any=None' for t in attrs)
         f.write(f"def {o}(*c, {attrs_str}, **kwargs): ...\n")
