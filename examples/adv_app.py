5271ef5e0efce76b2a9b1706e1396f4baa09a6f9
kklein <kleinkevin.lu@gmail.com>
2024-08-23 22:03:38 +0200

    Fix import in adv example.

--- examples/adv_app.py
+++ examples/adv_app.py
@@ -15,7 +15,7 @@ from fasthtml.common import (
     # These are the HTML components we use in this app
     A, AX, Button, Card, CheckboxX, Container, Div, Form, Grid, Group, H1, H2, Hidden, Input, Li, Main, Script, Style, Textarea, Title, Titled, Ul,
     # These are FastHTML symbols we'll use
-    Beforeware, fast_app, SortableJS, fill_form, picolink, serve,
+    Beforeware, FastHTML, fast_app, SortableJS, fill_form, picolink, serve,
     # These are from Starlette, Fastlite, fastcore, and the Python stdlib
     FileResponse, NotFoundError, RedirectResponse, database, patch, dataclass
 )
