fafd0061d492037028a84ccf235a7cb4961af716
Philip Nuzhnyi <philip.nuzhnyy@gmail.com>
2024-09-23 18:49:04 +0100

    add mk_pyi

--- tools/mk_pyi.py
+++ tools/mk_pyi.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from fasthtml.common import *
-from fasthtml.components import _all_
+from fasthtml.components import _all_, hx_attrs_annotations
 from fastcore.py2pyi import create_pyi
 
 create_pyi('fasthtml/core.py', 'fasthtml')
@@ -12,5 +12,5 @@ with open('fasthtml/components.pyi', 'a') as f:
     f.write(f"def ft_hx(tag: str, *c, {attrs_str}, **kwargs): ...\n")
     for o in _all_:
         attrs = (['name'] if o.lower() in named else []) + hx_attrs
-        attrs_str = ', '.join(f'{t}:Any=None' for t in attrs)
+        attrs_str = ', '.join(f'{t}:{"Any" if t not in hx_attrs_annotations else str(hx_attrs_annotations[t]).replace("typing.","")}=None' for t in attrs)
         f.write(f"def {o}(*c, {attrs_str}, **kwargs): ...\n")
