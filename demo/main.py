f6f507365356e7e95283a124f41b850633aba041
nick leung <nickleung+gh@gmail.com>
2025-08-17 16:53:30 -0700

    fix: rename 'title' DB field to 'task' to avoid fast_app() title kwarg collision

--- demo/main.py
+++ demo/main.py
@@ -3,18 +3,18 @@ from fasthtml.common import *
 def render(item:'Todo'):
     id = f'todo-{item.id}'
     dellink = AX('Delete', hx_delete=f'/todo/{item.id}', target_id=id, hx_swap='delete')
-    return Li(item.title, dellink, id=id)
+    return Li(item.task, dellink, id=id)
 
 auth = user_pwd_auth(user='s3kret', skip=[r'/favicon\.ico', r'/static/.*', r'.*\.css'])
 app,rt,todos,Todo = fast_app(
     'data/tbl.db', middleware=[auth], render=render,
-    id=int, title=str, pk='id')
+    id=int, task=str, pk='id')
 
 @rt("/")
 async def get(request):
     new_frm = Form(hx_post='/', target_id='todo-list', hx_swap='beforeend')(
         Group(
-            Input(name='title', placeholder='Title'),
+            Input(name='task', placeholder='Task'),
             Button('Add')
         )
     )
