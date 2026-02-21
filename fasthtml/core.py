21b62eaeed0631e9183e70cefbe9462306f6a9b6
Jeremy Howard <github@jhoward.fastmail.fm>
2026-02-21 12:18:23 +1000

    fixes #832

--- fasthtml/core.py
+++ fasthtml/core.py
@@ -170,6 +170,7 @@ class ApiReturn:
     async def __from_request__(cls, data, req): return cls(req.headers.get('accept')=='application/json')
     def __init__(self, isapi=False): self.isapi = isapi
     def __call__(self, norm, **kw): return kw if self.isapi else norm
+    def __bool__(self): return bool(self.isapi)
 
 # %% ../nbs/api/00_core.ipynb #7cc39ba9
 class JSONResponse(JSONResponseOrig):
