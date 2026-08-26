1050dc2681ac4ec3a932e6af1f6dea476985bde2
Jeremy Howard <github@jhoward.fastmail.fm>
2026-08-27 08:58:03 +1000

    Update CLI flags for fastcore.script hyphenation

--- tools/update.sh
+++ tools/update.sh
@@ -2,5 +2,4 @@
 python tools/mk_pyi.py
 llms_txt2ctx nbs/llms.txt --optional true > nbs/llms-ctx-full.txt
 llms_txt2ctx nbs/llms.txt > nbs/llms-ctx.txt
-pysym2md --output_file nbs/apilist.txt fasthtml --include_no_docstring
-
+pysym2md --output-file nbs/apilist.txt fasthtml --include-no-docstring
